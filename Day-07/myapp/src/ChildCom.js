3e1b4212ae26af8c2c413ef4666f4c38ee3e3efd
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2024-11-21 14:06:33 +0000

    ChildCom is updated

--- Day-07/myapp/src/ChildCom.js
+++ Day-07/myapp/src/ChildCom.js
@@ -1,6 +1,10 @@
-export default function ChildCom(){
+export default function ChildCom(props){
     return(
         <>
+        <h3>Hello React, I'm Child</h3>
+        {
+            PaymentResponse.setfun("My name is Bob")
+        }
         </>
     )
 }
